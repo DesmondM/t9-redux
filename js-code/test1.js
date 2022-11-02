ad0ce5160937a7b95cf3f6908cb84308dff32f62
Desmond <desmondmpofu@gmail.com>
2022-11-02 23:34:50 +0200

    add

---
+++ js-code/test1.js
@@ -0,0 +1,5 @@
+
+let a =0;
+
+for(a; a<5; a++);
+console.log(a)
---
+++ js-code/test2.js
@@ -0,0 +1,8 @@
+
+const func1 =a=>b=>c=>{
+    console.log(a+b+c)
+    return a+b+c
+}
+
+console.log(func1(10,20,30))
+console.log(func1(10,20,30))
